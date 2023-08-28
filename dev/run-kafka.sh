b0183e62110fd6ffbd54565aee286fb2b9e05b1a
zoukang <zoukang89105@163.com>
2023-08-28 11:21:17 +0800

    增加一个开发工具脚本dev/run-kafka.sh，用于启动kafka用于开发调试

---
+++ dev/run-kafka.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Copyright 2023 zoukang, All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# 作用：启动一个开发阶段用于测试的kafka
+
+if [ ! -d .tmp ]; then
+    mkdir .tmp
+fi
+
+if [ ! -d .tmp/kafka ]; then
+    if [ ! -f .tmp/kafka_2.13-3.5.1.tgz  ]; then
+        echo "Going to download kafka, please wait!"
+        if ! wget https://mirror.tuna.tsinghua.edu.cn/apache/kafka/3.5.1/kafka_2.13-3.5.1.tgz --output-document .tmp/kafka_2.13-3.5.1.tgz; then
+              echo "Download kafka failed, please try again later!"
+              exit 1
+        fi
+    fi
+    tar -zxvf .tmp/kafka_2.13-3.5.1.tgz -C .tmp/
+    mv .tmp/kafka_2.13-3.5.1 .tmp/kafka
+
+    cd .tmp/kafka || exit 1
+
+    cp config/server.properties config/server.properties.backup
+    cp config/zookeeper.properties config/zookeeper.properties.backup
+
+    sed -i 's/^log.dirs=.*$/log.dirs=data-kafka/g' config/server.properties
+    sed -i 's/^dataDir=.*$/dataDir=data-zookeeper/g' config/zookeeper.properties
+
+    mkdir data-kafka
+    mkdir data-zookeeper
+    cd ../..
+fi
+
+cd .tmp/kafka || exit 1
+
+gnome-terminal --working-directory "$(pwd)" --title zookeeper -- bin/zookeeper-server-start.sh config/zookeeper.properties
+
+sleep 2
+
+gnome-terminal --working-directory "$(pwd)" --title kafka -- bin/kafka-server-start.sh config/server.properties
