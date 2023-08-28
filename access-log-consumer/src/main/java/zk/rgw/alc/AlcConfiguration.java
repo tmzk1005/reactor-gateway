df44f5ffd0a33ee2bcbaa555abd037f1745635b6
zoukang <zoukang89105@163.com>
2023-08-28 14:18:20 +0800

    新增一个access-log-consumer模块，用于读取kafka中的AccessLog写入mongodb

---
+++ access-log-consumer/src/main/java/zk/rgw/alc/AlcConfiguration.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2023 zoukang, All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package zk.rgw.alc;
+
+import lombok.Getter;
+import lombok.Setter;
+import picocli.CommandLine;
+
+import zk.rgw.common.conf.CommonConfiguration;
+
+@SuppressWarnings("java:S1075")
+// @formatter:off
+@CommandLine.Command(
+        name = "reactor-access-log-consumer",
+        version = "0.0.1",
+        mixinStandardHelpOptions = true,
+        description = "A kafka consumer application, read access log written by gateway, and then write to mongodb.",
+        usageHelpAutoWidth = true,
+        showDefaultValues = true
+)
+// @formatter:on
+public class AlcConfiguration extends CommonConfiguration {
+
+    private static final String DEFAULT_CONF_FILE = "conf/rgw-access-log-consumer.properties";
+
+    public AlcConfiguration() {
+        this.confFile = DEFAULT_CONF_FILE;
+    }
+
+    @Getter
+    @Setter
+    @CommandLine.Option(names = "--kafka.bootstrap.servers", description = "The kafka bootstrap server used to read access log.")
+    private String kafkaBootstrapServers = "127.0.0.1:9092";
+
+}
