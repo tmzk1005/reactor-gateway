f7735819a8ade00666727a1eadb14069ae9c4ded
zoukang <zoukang89105@163.com>
2023-09-13 17:18:07 +0800

    扩展http协议代理插件，支持动态决定上游uri，以便支持自定义的负债均衡策略或者灰度发布策略

--- plugins/filter-plugins/proxy-http/src/main/java/zk/rgw/plugin/filter/proxyhttp/ProxyHttpFilter.java
+++ plugins/filter-plugins/proxy-http/src/main/java/zk/rgw/plugin/filter/proxyhttp/ProxyHttpFilter.java
@@ -20,20 +20,30 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 import io.netty.handler.codec.http.HttpHeaderNames;
 import io.netty.handler.codec.http.HttpResponseStatus;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Mono;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.http.server.HttpServerRequest;
 import reactor.netty.http.server.HttpServerResponse;
 
+import zk.rgw.common.exception.RgwException;
+import zk.rgw.common.exception.RgwRuntimeException;
 import zk.rgw.common.util.EnvNameExtractUtil;
+import zk.rgw.common.util.ObjectUtil;
 import zk.rgw.plugin.api.Exchange;
 import zk.rgw.plugin.api.filter.FilterChain;
 import zk.rgw.plugin.api.filter.JsonConfFilterPlugin;
@@ -41,7 +51,12 @@ import zk.rgw.plugin.exception.PluginConfException;
 import zk.rgw.plugin.util.ExchangeUtil;
 import zk.rgw.plugin.util.ResponseUtil;
 
+@Slf4j
 public class ProxyHttpFilter implements JsonConfFilterPlugin {
+
+    private static final String ENGINE_NAME = "groovy";
+
+    private static final String FUNC_NAME = "decideUri";
 
     private static final HttpClient HTTP_CLIENT = HttpClient.create();
 
@@ -69,7 +84,7 @@ public class ProxyHttpFilter implements JsonConfFilterPlugin {
 
         envNames = EnvNameExtractUtil.extract(proxyConf.upstreamEndpoint);
 
-        if (envNames.isEmpty()) {
+        if (envNames.isEmpty() && Objects.nonNull(proxyConf.getUpstreamEndpoint())) {
             try {
                 uri = new URI(proxyConf.getUpstreamEndpoint());
             } catch (URISyntaxException exception) {
@@ -82,28 +97,12 @@ public class ProxyHttpFilter implements JsonConfFilterPlugin {
     @Override
     public Mono<Void> filter(Exchange exchange, FilterChain chain) {
         HttpServerRequest request = exchange.getRequest();
-        URI uriToUse = uri;
 
-        if (Objects.isNull(uriToUse)) {
-            String uriStr = proxyConf.upstreamEndpoint;
-            Map<String, String> environment = ExchangeUtil.getEnvironment(exchange);
-            for (String envName : envNames) {
-                String value = environment.get(envName);
-                if (Objects.isNull(value)) {
-                    return ResponseUtil.sendError(
-                            exchange.getResponse(),
-                            "ProxyHttpFilter error: required environment variable named " + envName + " not found."
-                    );
-                }
-                uriStr = uriStr.replace("{" + envName + "}", value);
-            }
-
-            try {
-                uriToUse = new URI(uriStr);
-            } catch (URISyntaxException exception) {
-                String message = "ProxyHttpFilter error: failed to parse " + uriStr + " to URI";
-                return ResponseUtil.sendError(exchange.getResponse(), message);
-            }
+        URI uriToUse;
+        try {
+            uriToUse = decideUpstreamUri(exchange);
+        } catch (RgwException rgwException) {
+            return ResponseUtil.send(exchange.getResponse(), rgwException.getMessage());
         }
 
         return this.httpClient.headers(headers -> {
@@ -122,11 +121,103 @@ public class ProxyHttpFilter implements JsonConfFilterPlugin {
         ).then();
     }
 
+    private URI decideUpstreamUri(Exchange exchange) throws RgwException {
+        if (Objects.nonNull(uri)) {
+            // 固定的uri
+            return uri;
+        } else if (ObjectUtil.isEmpty(proxyConf.upstreamEndpointDecideFuncDef)) {
+            // 含有环境变量的uri
+            return decideUpstreamUriByEnv(exchange);
+        } else {
+            // 由用户配置的groovy脚本动态决定uri, 用户可以自定义
+            return decideUpstreamUriByScript(exchange);
+        }
+    }
+
+    private URI decideUpstreamUriByEnv(Exchange exchange) throws RgwException {
+        String uriStr = proxyConf.upstreamEndpoint;
+        Map<String, String> environment = ExchangeUtil.getEnvironment(exchange);
+        for (String envName : envNames) {
+            String value = environment.get(envName);
+            if (Objects.isNull(value)) {
+                throw new RgwException("ProxyHttpFilter error: required environment variable named " + envName + " not found.");
+            }
+            uriStr = uriStr.replace("{" + envName + "}", value);
+        }
+
+        try {
+            return new URI(uriStr);
+        } catch (URISyntaxException exception) {
+            String message = "ProxyHttpFilter error: failed to parse " + uriStr + " to URI";
+            throw new RgwException(message);
+        }
+    }
+
+    private URI decideUpstreamUriByScript(Exchange exchange) throws RgwException {
+        ScriptEngine scriptEngine = new ScriptEngineManager().getEngineByName(ENGINE_NAME);
+
+        if (proxyConf.useRequestHeaders) {
+            Map<String, String> requestHeaders = new HashMap<>();
+            exchange.getRequest().requestHeaders().forEach(entry -> requestHeaders.put(entry.getKey(), entry.getValue()));
+            scriptEngine.put("requestHeaders", requestHeaders);
+        }
+
+        if (proxyConf.useClientRequestUriPath) {
+            scriptEngine.put("clientRequestUriPath", exchange.getRequest().uri());
+        }
+
+        if (proxyConf.useClientRequestUriParameters) {
+            scriptEngine.put("clientRequestUriParameters", ExchangeUtil.getQueryParams(exchange));
+        }
+
+        if (proxyConf.useEnvironmentVariables) {
+            Map<String, String> environment = ExchangeUtil.getEnvironment(exchange);
+            scriptEngine.put("environment", Map.copyOf(environment));
+        }
+
+        try {
+            scriptEngine.eval(proxyConf.upstreamEndpointDecideFuncDef);
+        } catch (ScriptException exception) {
+            String msg = "HTTP协议代理异常：解析动态设置上游uri的groovy方法定义失败";
+            log.error("{}", msg, exception);
+            throw new RgwRuntimeException(msg);
+        }
+
+        Invocable invocable = (Invocable) scriptEngine;
+
+        Object result;
+
+        try {
+            result = invocable.invokeFunction(FUNC_NAME);
+        } catch (Exception exception) {
+            String msg = "HTTP协议代理异常：执行动态设置上游uri的groovy方法异常";
+            log.error("{}", msg, exception);
+            throw new RgwRuntimeException(msg);
+        }
+
+        if (result instanceof URI resultUri) {
+            return resultUri;
+        } else {
+            String msg = "HTTP协议代理异常：动态设置上游uri的groovy方法返回结果不是一个URI对象";
+            throw new RgwException(msg);
+        }
+    }
+
     @Getter
     @Setter
     public static class ProxyConf {
 
         private String upstreamEndpoint;
+
+        private String upstreamEndpointDecideFuncDef;
+
+        private boolean useRequestHeaders;
+
+        private boolean useClientRequestUriPath;
+
+        private boolean useClientRequestUriParameters;
+
+        private boolean useEnvironmentVariables;
 
         private int timeout = Integer.MAX_VALUE;
 
